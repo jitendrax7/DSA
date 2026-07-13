5ad4c4cab893792cfe10702524fef8a744951f5a
jitendra singh <rathourjitendrasingh472@gmail.com>
2026-07-13 10:24:14 +0530

    Time: 0 ms (100%), Space: 8.5 MB (18.24%) - LeetHub

--- 1291-sequential-digits/1291-sequential-digits.cpp
+++ 1291-sequential-digits/1291-sequential-digits.cpp
@@ -1,18 +1,44 @@
+// class Solution {
+// public:
+//     vector<int> sequentialDigits(int low, int high) {
+//         vector<int> ans;
+//         for(int i=1;i<=9;i++){
+//             int num = i;
+//             for(int j =i+1;j<=9;j++){
+//                 num = num*10+j;
+//                 if(num>= low && num<= high){
+//                     ans.push_back(num);
+//                 }
+//             }
+//         }
+
+//         sort(ans.begin(),ans.end());
+//         return ans;
+//     }
+// };
+
+
 class Solution {
 public:
     vector<int> sequentialDigits(int low, int high) {
         vector<int> ans;
-        for(int i=1;i<=9;i++){
-            int num = i;
-            for(int j =i+1;j<=9;j++){
-                num = num*10+j;
-                if(num>= low && num<= high){
-                    ans.push_back(num);
-                }
+
+        queue<int> q;
+        for(int i =1;i<9;i++){
+            q.push(i);
+        }
+        while(!q.empty()){
+            int num = q.front();
+            q.pop();
+            if(num>= low && num<= high){
+                ans.push_back(num);
+            }
+            int lstdigit = num%10;
+            if(num<=high && lstdigit<9 ){
+                q.push(num*10+lstdigit+1);
             }
         }
 
-        sort(ans.begin(),ans.end());
         return ans;
     }
 };
