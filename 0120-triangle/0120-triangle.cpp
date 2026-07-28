c7f4bb4e1e32dad2f4a6e6155edd9010c8ab2250
jitendra singh <rathourjitendrasingh472@gmail.com>
2026-07-28 18:06:21 +0530

    Time: 3 ms (13.15%), Space: 12.9 MB (70.15%) - LeetHub

---
+++ 0120-triangle/0120-triangle.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int f(vector<vector<int>>& triangle, int row, int col, vector<vector<int>> & dp ){   // space O(n*n)
+
+        if(row==triangle.size()-1) return triangle[row][col];
+
+        if(dp[row][col]!=INT_MIN) return dp[row][col];
+        int down = triangle[row][col] + f(triangle,row+1, col, dp);
+        int downright = triangle[row][col] + f(triangle,row+1, col+1, dp);
+
+        return dp[row][col] = min(down,downright);
+    }
+
+    int minimumTotal(vector<vector<int>>& triangle) {     
+
+        // memo.. + recursion 
+        // vector<vector<int>> dp(triangle.size(), vector<int>(triangle[triangle.size()-1].size(),INT_MIN));
+        // return f(triangle, 0,0, dp);
+
+       // space n , tabulation 
+        int n = triangle.size();
+        vector<int> dp (n);
+        dp = triangle[n-1];
+        for(int i =n-2;i>=0;i--){
+            vector<int> curr(n,0);
+            for(int j =0;j<triangle[i].size();j++){
+                int down = triangle[i][j]+dp[j];
+                int downleft = triangle[i][j]+dp[j+1];
+                curr[j] = min(down, downleft);
+            }
+            dp = curr;
+        }
+        return dp[0];
+    }
+};
