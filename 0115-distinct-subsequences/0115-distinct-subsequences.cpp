7d621e70f686309f91749abe996db8ddf667145e
jitendra singh <rathourjitendrasingh472@gmail.com>
2026-09-06 11:34:54 +0530

    Time: 15 ms (82.67%), Space: 8.8 MB (95.82%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -14,8 +14,8 @@ public:
     //     return dp[i][j] = take+nottake;
     // }
     int numDistinct(string s, string t) {
-        int n =s.size();
-        int m =t.size();
+        int n = s.size();
+        int m = t.size();
 
         // recursion + memoazation
         // vector<vector<int>> dp(n,vector<int>(m,-1));
@@ -26,18 +26,18 @@ public:
         // for(int i=0;i<=n;i++) dp[i][m] = 1;
 
         // memory optimization
-        vector<int> dp(m+1,0);
-        vector<int> curr(m+1,0);
-        dp[m] =1;
+        vector<int> dp(m + 1, 0);
+        vector<int> curr(m + 1, 0);
+        dp[m] = 1;
         curr[m] = 1;
-        for(int i =n-1;i>=0;i--){
-            for(int j=m-1;j>=0;j--){
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = m - 1; j >= 0; j--) {
                 int nottake = dp[j];
-                int take =0;
-                if(s[i]==t[j]){
-                    take = dp[j+1];
+                int take = 0;
+                if (s[i] == t[j]) {
+                    take = dp[j + 1];
                 }
-                curr[j] = 1LL*take+nottake;
+                curr[j] = 1LL * take + nottake;
             }
             dp = curr;
         }
