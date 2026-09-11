910f20cc644685f3f654f06b65f19cd37c1de862
jitendra singh <rathourjitendrasingh472@gmail.com>
2026-09-11 11:17:46 +0530

    Time: 4 ms (79.08%), Space: 31 MB (33.29%) - LeetHub

--- 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.cpp
+++ 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.cpp
@@ -4,7 +4,6 @@ public:
         int n = digits.size();
         unordered_set<int> st;
         for(int i =0;i<n;i++){
-            // if (digits[i] == 0) continue;
             for(int j =0;j<n;j++){
                 for(int k =0;k<n;k++){
                     if (k == i || k == j || i==j) continue;
